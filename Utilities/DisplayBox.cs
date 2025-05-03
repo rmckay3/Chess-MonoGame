540244901eb30737239562adf84bb910a3e18565
Robert McKay <robertmckay0725@gmail.com>
2025-05-03 14:05:33 -0400

    Adding Button logic

--- Utilities/DisplayBox.cs
+++ Utilities/DisplayBox.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Utilities.Extensions;
+using Managers;
 
 namespace Utilities {
     public class DisplayBox {
@@ -16,6 +20,7 @@ namespace Utilities {
         private int _y = 0;
         private SpriteFont _headerFont;
         private SpriteFont _bodyFont;
+        private List<Button> _buttons;
 
         private Rectangle background;
         private Rectangle body;
@@ -23,6 +28,8 @@ namespace Utilities {
         private Texture2D bodyTexture;
         private Vector2 fontOriginHeader;
         private Vector2 fontOriginBody;
+        private Vector2 headerLocation;
+        private Vector2 bodyLocation;
 
         public string Header { get { return _header;} }
         public string Body { get { return _body;} }
@@ -106,14 +113,95 @@ namespace Utilities {
             this.bodyTexture.SetData(Enumerable.Repeat(new Color(Color.LightGray, 1.0f), this._width * (this._height - this._margin)).ToArray());
             this.fontOriginHeader = _headerFont.MeasureString(this._header) / 2;
             this.fontOriginBody = _bodyFont.MeasureString(this._body) / 2;
+            this.headerLocation = new Vector2(this._x + (this._width / 2), this._y + (this._margin / 2));
+            this.bodyLocation = new Vector2(this._x + (this._width / 2), this._y + ((this._height + this._margin) / 2));
+
+            this._buttons = new List<Button>()
+            {
+                new Button(graphicsDevice, (this._x + (this._width / 2)) - 30, (this._y + (9*(this._height - this._margin) / 10)) + 10, 60, 20, "OK", this._bodyFont, Color.DarkGray, () => { return; })
+            };
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(this._headerFont, this._header, new Vector2((2*this.background.X + this._width) / 2, this.background.Y + 10), Color.Black, 0, this.fontOriginHeader, 1.0f, SpriteEffects.None, 1f);
-            spriteBatch.DrawString(this._bodyFont, this._body, new Vector2((2*this.body.X + this._width) / 2, (2*this.body.Y + this._height - this._margin) / 2), Color.Black, 0, this.fontOriginBody, 1.0f, SpriteEffects.None, 1f);
-            spriteBatch.DrawWithBorder(backgroundTexture, this.background, 6, Color.DarkGray, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.98f);
-            spriteBatch.DrawWithBorder(bodyTexture, this.body, 6, Color.DarkGray, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.99f);
+            spriteBatch.DrawString(this._headerFont, this._header, this.headerLocation, Color.Black, 0, this.fontOriginHeader, 1.0f, SpriteEffects.None, 1f);
+            spriteBatch.DrawString(this._bodyFont, this._body, this.bodyLocation, Color.Black, 0, this.fontOriginBody, 1.0f, SpriteEffects.None, 1f);
+            spriteBatch.DrawWithBorder(backgroundTexture, this.background, 6, Color.Black, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.90f);
+            spriteBatch.DrawWithBorder(bodyTexture, this.body, 6, Color.Black, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.91f);
+
+            foreach (var button in this._buttons) {
+                button.Draw(spriteBatch);
+            }
+        }
+    }
+
+    public class Button {
+        private int _width = 0;
+        private int _height = 0;
+        private int _x = 0;
+        private int _y = 0;
+        private string _text = "";
+        private Action _onClick = null;
+        private SpriteFont textFont;
+
+        private Rectangle btnBackground;
+        private Texture2D btnTexture;
+        private Color btnColor;
+        private Vector2 fontOrigin;
+        private Vector2 textLocation;
+
+        public int Width { get { return _width;} }
+        public int Height { get { return _height;} }
+        public int X { get { return _x;} }
+        public int Y { get { return _y;} }
+        public string Text { get { return _text;} }
+        public Action Click { get { return _onClick; } }
+
+        public Button(GraphicsDevice graphicsDevice, int x, int y, int width, int height, string text, SpriteFont textFont, Color color) {
+            this._width = width;
+            this._height = height;
+            this._x = x;
+            this._y = y;
+            this._text = text;
+            this.textFont = textFont;
+            this.btnColor = color;
+            this.textFont = textFont;
+            
+            Initialize(graphicsDevice);
+        }
+
+        public Button(GraphicsDevice graphicsDevice, int x, int y, int width, int height, string text, SpriteFont textFont, Color color, Action onClick) {
+            this._width = width;
+            this._height = height;
+            this._x = x;
+            this._y = y;
+            this._text = text;
+            this.textFont = textFont;
+            this.btnColor = color;
+            this._onClick = onClick;
+
+            Initialize(graphicsDevice);
+        }
+
+        private void Initialize(GraphicsDevice graphicsDevice) {
+            this.btnBackground = new Rectangle(this._x, this._y, this._width, this._height);
+            this.btnTexture = new Texture2D(graphicsDevice, this._width, this._height);
+            this.btnTexture.SetData(Enumerable.Repeat(new Color(this.btnColor, 1.0f), this._width * this._height).ToArray());
+            this.fontOrigin = this.textFont.MeasureString(this._text) / 2;
+            this.textLocation = new Vector2(this._x + (this._width / 2), this._y + (this._height / 2));
+        }
+
+        public bool IsClicked(InputStateManager inputStateManager) {
+
+            return  inputStateManager.CurrentMouseState.X >= this._x && 
+                    inputStateManager.CurrentMouseState.X <= this._x + this._width &&
+                    inputStateManager.CurrentMouseState.Y >= this._y &&
+                    inputStateManager.CurrentMouseState.Y <= this._y + this._height;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.DrawString(this.textFont, this._text, this.textLocation, Color.Black, 0, this.fontOrigin, 0.7f, SpriteEffects.None, 1f);
+            spriteBatch.DrawWithBorder(this.btnTexture, this.btnBackground, 6, Color.Black, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.93f);
         }
     }
 }
--- Utilities/Extensions/SpriteBatchExtensions.cs
+++ Utilities/Extensions/SpriteBatchExtensions.cs
@@ -23,12 +23,20 @@ namespace Utilities.Extensions {
                 destinationRectangle.Height - borderWidth
             );
 
+            Color[] colors = new Color[newDestinationRectangle.Width * newDestinationRectangle.Height]; 
+            Color[] test = new Color[destinationRectangle.Width * destinationRectangle.Height];
+            int startIndexForCopy = (destinationRectangle.Width * destinationRectangle.Height) - colors.Length;
             Texture2D updatedTexture = new Texture2D(texture.GraphicsDevice, texture.Width - borderWidth, texture.Height - borderWidth);
+            //texture.GetData(colors, startIndexForCopy + 1, newDestinationRectangle.Width * newDestinationRectangle.Height);
+            texture.GetData(test);
+            colors = test.Skip(startIndexForCopy).ToArray();
+            updatedTexture.SetData(colors);
             Texture2D borderTexture = new Texture2D(texture.GraphicsDevice, texture.Width, texture.Height);
             borderTexture.SetData(Enumerable.Repeat(borderColor, borderTexture.Width * borderTexture.Height).ToArray());
 
+            float newDepth = layerDepth - 0.001f;
             // Draw border rectangle using a blank Texture and letting the Rectangle fill in the color
-            spriteBatch.Draw(borderTexture, destinationRectangle, null, color, rotation, origin, effects, layerDepth - 0.001f);
+            spriteBatch.Draw(borderTexture, destinationRectangle, null, color, rotation, origin, effects, newDepth);
 
             // Reuse existing draw function, we are simply drawing a small border around the pulled in sprite
             spriteBatch.Draw(updatedTexture, newDestinationRectangle, null, color, rotation, origin, effects, layerDepth);
